241247196b713167d18af5e7530dcdaad7b676ad
Sara Tayel <132705234+Sara-code-beep@users.noreply.github.com>
2023-12-19 08:43:41 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+window.onload=function(){
+    document.querySelector("h1").style.color="black"
+}
+
+
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
